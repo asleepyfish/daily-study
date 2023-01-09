f9f4331dfe27015ee1b6b8754645bdfb88ddac9b
zhoujh42045 <zhoujh42045@hundsun.com>
2023-01-09 16:44:51 +0800

    增加新模块

---
+++ NewTec/tec-admin/src/main/java/com/asleepyfish/admin/NewTecStarter.java
@@ -0,0 +1,16 @@
+package com.asleepyfish.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zhoujh42045
+ * @Date: 2023/1/9 13:20
+ * @Description: NewTecStarter
+ */
+@SpringBootApplication
+public class NewTecStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(NewTecStarter.class, args);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-
+#1.端口
+```txt
+SpringBoot：8080
+Java：8081
+Spring：8082
+Design：8083
+NewTec：8084
+```
